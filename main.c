5f1ceefc50cd53b9960fe67e86ce911757c13d56
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-01-18 20:26:10 +0300

    Sync with blog

--- main.c
+++ main.c
@@ -1,46 +1,34 @@
 #include <stdint.h>
 
 
-#define     __IO    volatile
-
-
-typedef struct GPIO_struct
+typedef struct SGPIO
 {
-  __IO uint8_t ODR;
-  __IO uint8_t IDR;
-  __IO uint8_t DDR;
-  __IO uint8_t CR1;
-  __IO uint8_t CR2;
-} GPIO_TypeDef;
+    volatile uint8_t ODR;
+    volatile uint8_t IDR;
+    volatile uint8_t DDR;
+    volatile uint8_t CR1;
+    volatile uint8_t CR2;
+} GPIO;
 
-#define GPIOB_BaseAddress       0x5005
-#define GPIOB ((GPIO_TypeDef *) GPIOB_BaseAddress)
+#define GPIOB_BaseAddress 0x5005
+#define GPIOB ((GPIO*) GPIOB_BaseAddress)
 
 
 static void delay(uint32_t t) {
-    while (t--) {
-    }
+    while (t--)
+        ;
 }
 
-static volatile uint32_t dummy = 0;
-
 void main(void) {
-
-
     GPIOB->DDR = 0xFF;
     GPIOB->CR1 = 0xFF;
     GPIOB->CR2 = 0xFF;
-    while(1) {
+
+    for (;;) {
         GPIOB->ODR = 0xFF;
         delay(10000UL);
         GPIOB->ODR = 0;
-        if (dummy % 2) {
-            delay(10000UL);
-        }
-        else {
-            delay(10001UL);
-        }
-        dummy++;
+        delay(10000UL);
     }
 }
 
